661a1ba2f1fa27d67af05d31950e8d667af17c1d
peace-among-worlds <34693070+peace-among-worlds@users.noreply.github.com>
2021-06-22 21:29:52 +1200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'tachyons';
+import { robots } from './robots';
+// curly braces above to indicate not default
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
